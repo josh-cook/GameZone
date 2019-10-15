7936091283d60e3eb209f1fa1e82fd2ff4736de7
Joshua <joshua.cook@onthebeach.co.uk>
2019-10-15 08:38:42 +0100

    Reformat documents

--- src/GameZone/Program.cs
+++ src/GameZone/Program.cs
@@ -1,35 +1,30 @@
 ﻿using System;
 
-namespace GameZone
-{
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("Which Game Would you like to play? (Pick a number)");
+namespace GameZone {
+    class Program {
+        static void Main () {
+            Console.WriteLine ("Which Game Would you like to play? (Pick a number)");
             string[] games = {
-                "1.Simone Says", "2.Suspend Person"
-                };
-            for (int i = 0; i < games.Length; i++)
-            {
-                Console.WriteLine("{0,-20}", games[i]);
+                "1.Simone Says",
+                "2.Suspend Person",
+            };
+            foreach (string game in games) {
+                Console.WriteLine ("{0,-20}", game);
             }
 
-            string userInput = Console.ReadLine();
-            switch (userInput)
-            {
+            string userInput = Console.ReadLine ();
+            switch (userInput) {
                 case "1":
-                    SimoneSays.Play();
+                    SimoneSays.Play ();
                     break;
                 case "2":
-                    SuspendPerson.Play();
+                    SuspendPerson.Play ();
                     break;
                 default:
-                    Console.WriteLine("You didn't choose a correct number...");
-                    Main();
+                    Console.WriteLine ("You didn't choose a correct number...");
+                    Main ();
                     break;
             }
         }
     }
 }
-
--- src/GameZone/SimoneSays.cs
+++ src/GameZone/SimoneSays.cs
@@ -1,42 +1,35 @@
 using System;
 
-public class SimoneSays
-{
-    public static void Play()
-    {
-        Console.WriteLine("Welcome to Simone Says, a game about following orders.");
-        string[] tasks = new string[]{
-                "Pick your nose",
-                "Jump",
-                "Waggle your finger",
-                "Sleep"
-            };
+public class SimoneSays {
+    public static void Play () {
+        Console.WriteLine ("Welcome to Simone Says, a game about following orders.");
+        string[] tasks = new string[] {
+            "Pick your nose",
+            "Jump",
+            "Waggle your finger",
+            "Sleep"
+        };
 
-        Random rand = new Random();
-        int seed = rand.Next(2);
+        Random rand = new Random ();
+        int seed = rand.Next (2);
         bool simoneSaidIt = (seed == 0);
-        int commandSeed = rand.Next(tasks.Length);
-        if (simoneSaidIt)
-        {
-            Console.WriteLine("Simone Says:...");
+        int commandSeed = rand.Next (tasks.Length);
+        if (simoneSaidIt) {
+            Console.WriteLine ("Simone Says:...");
         }
         string selectedTask = tasks[commandSeed];
-        Console.WriteLine($"{selectedTask}");
+        Console.WriteLine ($"{selectedTask}");
 
-        string input = Console.ReadLine();
+        string input = Console.ReadLine ();
 
-        if (simoneSaidIt && input.Contains(selectedTask))
-        {
-            Console.WriteLine("Congrats");
-        }
-        else if (!simoneSaidIt && input.Contains(selectedTask))
-        {
-            Console.WriteLine("You've knacked it");
+        if (simoneSaidIt && input.Contains (selectedTask)) {
+            Console.WriteLine ("Congrats");
+        } else if (!simoneSaidIt && input.Contains (selectedTask)) {
+            Console.WriteLine ("You've knacked it");
         }
 
-        if (!simoneSaidIt && input.Contains(""))
-        {
-            Console.WriteLine("You're the best");
+        if (!simoneSaidIt && input.Contains ("")) {
+            Console.WriteLine ("You're the best");
         }
     }
 }
