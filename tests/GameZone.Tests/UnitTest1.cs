5064e2d53751d3809795d2b90bae42b9fa22975e
Joshua <joshua.cook@onthebeach.co.uk>
2019-10-15 09:39:08 +0100

    Add tests and solution/tests

--- tests/GameZone.Tests/UnitTest1.cs
+++ tests/GameZone.Tests/UnitTest1.cs
@@ -1,14 +1,11 @@
 using System;
 using Xunit;
 
-namespace GameZone.Tests
-{
-    public class UnitTest1
-    {
+namespace GameZone.Tests {
+    public class UnitTest1 {
         [Fact]
-        public void Test1()
-        {
-
+        public void Test1 () {
+            Assert.Equal (1, 1);
         }
     }
 }
